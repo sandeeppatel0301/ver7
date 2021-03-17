ca6b588ee292b4f8d2579b9581d39b175920610d
Sandeep <sandeep.patel4488@gmail.com>
2021-03-17 17:05:18 +0530

    Factory-faker seeder updated for dummy User Table data

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,10 +12,14 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        factory(App\User::class, 500)->create()->each(function($user){
+            $user->save();
+        });
+        /*
         User::create([
             'name' => 'Manjo',
             'email' => 'manoj@gmail.com',
             'password' => bcrypt('123456'),
-        ]);
+        ]);*/
     }
 }
