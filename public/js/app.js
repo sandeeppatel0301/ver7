b51ca5d5dbf959cb2249bea22970e52d3358f083
Sandeep <sandeep.patel4488@gmail.com>
2022-02-22 18:12:14 +0530

    initial develop branch commit

--- public/js/app.js
+++ public/js/app.js
@@ -37410,8 +37410,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\Learning\Ver7\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\Learning\Ver7\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\ASUS\Desktop\Exercises\ver7\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\ASUS\Desktop\Exercises\ver7\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
